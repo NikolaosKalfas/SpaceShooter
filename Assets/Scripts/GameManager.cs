0775d8ef7be7e8c585989f31b1d75a21d4e294e0
NikolaosKalfas <nikolaoskalfas1989@gmail.com>
2023-11-16 07:00:02 +0000

    GameOver sequence

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private bool _isGameOVer;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && _isGameOVer == true)
+        {
+            SceneManager.LoadScene(0); // Current Game Scene
+        }
+    }
+
+    public void GameOVer()
+    {
+        _isGameOVer = true;
+    }
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -17,11 +17,22 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private Text _gameOverText;
 
+    [SerializeField]
+    private Text _restartText;
+
+    private GameManager _gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
         _scoreText.text = "Score: " + 0;
         _gameOverText.gameObject.SetActive(false);
+        _gameManager = GameObject.Find("Game_Manager").GetComponent<GameManager>();
+
+        if (_gameManager == null)
+        {
+            Debug.LogError("GameManager is NULL.");
+        }
     }
 
     public void UpdateScore(int playerScore)
@@ -35,15 +46,23 @@ public class UIManager : MonoBehaviour
 
         if (currentLives == 0)
         {
-            _gameOverText.gameObject.SetActive(true);
-            StartCoroutine(GameOverFlickerRoutine());
+            GameOVerSequence();
         }
+    }
+
+    void GameOVerSequence()
+    {
+
+        _gameOverText.gameObject.SetActive(true);
+        _restartText.gameObject.SetActive(true);
+        StartCoroutine(GameOverFlickerRoutine());
     }
 
     IEnumerator GameOverFlickerRoutine()
     {
         while(true)
         {
+            _gameManager.GameOVer();
             _gameOverText.text = "GAME OVER";
             yield return new WaitForSeconds(0.5f);
             _gameOverText.text = "";
